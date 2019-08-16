4f0a056f4d664c17767053b4771dca4f0617007c
Greenbirch2007 <291109028@qq.com>
2019-08-16 21:01:18 +0800

    提交

--- GoEnv/ago.go
+++ GoEnv/ago.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+type Books struct {
+	title stirng
+	author string
+	subject string
+	book_id int
+}
+
 func main()  {
-	fmt.Println("zheshi~")
+	var Book1 Books  //声明Book1 为Books类型
+
+	Book1.title = "go 语言"
+	Book1.author = "www.runoob.com"
+	Book1.subject = "go语言"
+	Book1.book_id = 999
+
+	printBook(Book1)
+}
+
+func printBook(book Books)  {
+	fmt.Printf(book.title);
 }
