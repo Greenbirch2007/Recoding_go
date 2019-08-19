73ef79903d1bd45ec3495fa34606caf0fc7293e9
Greenbirch2007 <291109028@qq.com>
2019-08-19 15:09:44 +0800

    go标准库

--- GoEnv/src/s.go
+++ GoEnv/src/s.go
@@ -1,10 +1,5 @@
 package main
 
-import "io"
-
-type Reader interface {
-	Read(p []byte)(n int,err error)
-}
-type ReaderAt interface {
-	ReaderAt(p []byte,off int64)(n int,err error)
+func Contains(s,substr string) bool  {
+	return Index()
 }
