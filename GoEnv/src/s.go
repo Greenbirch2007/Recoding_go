7fae2699f2ebf05d4aa07184fae04c388230fd97
Greenbirch2007 <291109028@qq.com>
2019-08-21 16:38:54 +0800

    堆

--- GoEnv/src/s.go
+++ GoEnv/src/s.go
@@ -1,5 +1,46 @@
 package main
 
-func Contains(s,substr string) bool  {
-	return Index()
+import (
+	"fmt"
+	"sort"
+)
+
+type StuScore struct {
+	name string
+	score int
+
+}
+
+type StuScores []StuScore
+
+func (s StuScores) Len() int  {
+	return len(s)
+}
+
+func (s StuScores)Less(i,j int) bool  {
+	return s[i].score < s[j].score
+}
+
+func (s StuScores) Swap(i,j int )  {
+	s[i],s[j] = s[j],s[i]
+}
+
+func main()  {
+	stus　:= StuScores{
+		{"alan",95},
+		{"alan",95},
+		{"alan",95}
+	}
+	fmt.Println("Default")
+	for _,v := range stus{
+		fmt.Println(v.name,":",v.score)
+	}
+	fmt.Println()
+
+	sort.Sort(stus)
+	fmt.Println("Sorted:")
+	for _,v := range stud{
+		fmt.Println(v.name,":",v.score)
+	}
+	fmt.Println("IS Sorted?",sort.IsSorted(stud))
 }
