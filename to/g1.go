76cdd513d898d023d0651865235477f04d596840
Greenbirch2007 <291109028@qq.com>
2019-12-01 12:06:01 +0800

    go语言数组

--- to/g1.go
+++ to/g1.go
@@ -2,14 +2,25 @@ package main
 
 import "fmt"
 
-func main()  {
-		var a int =10
+//声明全局变量
 
-		for a<20{
-			if a==15 {
-			a = a+1
-			continue
-		}
-		fmt.Print("a的值为 %d \n",a)
-		a++}
+
+//函数定义－两数字相加
+
+func sum(a,b int) int  {
+	fmt.Printf("sum()函数中a = %d\n",a)
+	fmt.Printf("sum()函数中b = %d\n",b)
+	return a +b
+}
+
+func main()  {
+	//main函数中声明巨变
+	var a int =10
+	var b int = 20
+	var c int = 0
+
+	fmt.Printf("main()函数中a=%d\n",a)
+	c = sum(a,b)
+	fmt.Printf("main()函数中c=%d\n",c)
+
 }
