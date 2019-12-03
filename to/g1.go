c6035bff69b3e2a3de54e69730f9b77f220c81d9
Greenbirch2007 <291109028@qq.com>
2019-12-03 11:14:33 +0800

    Go语言菜鸟教程过完

--- to/g1.go
+++ to/g1.go
@@ -2,33 +2,23 @@ package main
 
 import "fmt"
 
-import (
-	_"github.com/go-sql-driver/mysql"
-	"database/sql"
-	"fmt"
-)
+func fibonacci(n int,c chan int)  {
+	x,y := 0,1
+	for i :=0;i<n;i++{
+		c <-x
+		x,y = y,x+y
+	}
+	close(c)
+}
 
 func main()  {
-	nums := []int {2,3,4}
-	sum := 0
-	for _,num := range nums{
-		sum += num
-	}
-
-	fmt.Println("sum:",sum)
-
-	for i,num := range nums{
-		if num == 3{
-			fmt.Println("index:",i)
-		}
-	}
-
-	kvs := map[string]string{"a":"apple","b":"banana"}
-	for k,v := range kvs{
-		fmt.Printf("%s -> %s \n",k,v)
-	}
-
-	for i,c := range  "go"{
-		fmt.Println(i,c)
+	c := make(chan int,10)
+	go fibonacci(cap(c),c)
+	// range函数遍历每个从通道接收到的数据，因为c在发送完
+	// 10个数据之后就关闭了通道，这里range函数在接收到10个
+	// 数据之后就结束了,如果上面的c通道不关闭，那么
+	// range函数就不会结束，从而在接收第11个数据的时候就阻塞了
+	for i := range c{
+		fmt.Println(i)
 	}
 }
