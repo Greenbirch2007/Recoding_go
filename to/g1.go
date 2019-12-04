6b4cdb7a2b1d04b6409d052f3ebf5073e7d9b287
Greenbirch2007 <291109028@qq.com>
2019-12-04 18:31:16 +0800

    Go语言常量和const关键字

--- to/g1.go
+++ to/g1.go
@@ -1,24 +1,19 @@
 package main
 
-import "fmt"
+//导入系统包
+import (
+	"flag"
+	"fmt"
+)
 
-func fibonacci(n int,c chan int)  {
-	x,y := 0,1
-	for i :=0;i<n;i++{
-		c <-x
-		x,y = y,x+y
-	}
-	close(c)
-}
+//定义命令行参数
+
+var mode = flag.String("mode","","porsdaffa")
 
 func main()  {
-	c := make(chan int,10)
-	go fibonacci(cap(c),c)
-	// range函数遍历每个从通道接收到的数据，因为c在发送完
-	// 10个数据之后就关闭了通道，这里range函数在接收到10个
-	// 数据之后就结束了,如果上面的c通道不关闭，那么
-	// range函数就不会结束，从而在接收第11个数据的时候就阻塞了
-	for i := range c{
-		fmt.Println(i)
-	}
+	// 解析命令行参数
+	flag.Parse()
+
+	//输出命令行采纳数
+	fmt.Println(*mode)
 }
