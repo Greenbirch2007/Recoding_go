86eae7a9ac95dae32d485439142fbfb1c42d8d91
Greenbirch2007 <291109028@qq.com>
2019-12-14 22:26:03 +0800

    切片操作

--- to/g1.go
+++ to/g1.go
@@ -2,25 +2,22 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 func main()  {
-	env := os.Environ()
-	procAttr := &os.ProcAttr{
-		Env:env,
-		Files:[]*os.File{
-			os.Stdin,
-			os.Stdout,
-			os.Stderr,
-
-		},
+	//定义一个数组
+	var myArray [10]int = [10]int{1,2,3,4,5,6,7,8,9,10}
+	//youArray := [6]int{6,6,6,6,6,6}
+	var mySlice []int = myArray[:5]
+	fmt.Println("Elements of myArray: ")
+	for _, v := range myArray {
+		fmt.Print(v, " ")
 	}
-	pid, err := os.StartProcess("/bin/ls",[]string{"ls","-l"},
-		procAttr)
-	if err != nil{
-		fmt.Printf("Error %v starting process!",err)
-		os.Exit(1)
+	fmt.Println("\nElements of mySlice: ")
+	for k, v := range mySlice {
+		fmt.Print(k,v)
 	}
-	fmt.Printf("The process id is %v",pid)
+	fmt.Println()
 }
+
+
