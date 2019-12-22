51e83c942d4c2b2886e9c920cb7343acfe72d6ac
Greenbirch2007 <291109028@qq.com>
2019-12-22 18:12:26 +0800

    Go语言入门——进阶语法篇（三）

--- to/t01.go
+++ to/t01.go
@@ -2,41 +2,28 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"go/ast"
 )
 
+type A struct {
+	Name string
+	id int64
+}
+
+type B struct {
+	A
+	Name string
+	num int
+}
+
+type C struct {
+	A
+	B
+}
 func main()  {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
-	go pump1(ch1)
-	go pump2(ch2)
-	go suck(ch1,ch2)
-	time.Sleep(1e9)
 
-
-
-}
-
-func pump1(ch chan int)  {
-	for i := 0;;i++{
-		ch <- i *2
-	}
-}
-
-func pump2(ch chan int)  {
-	for i := 0;;i++{
-		ch <- i +5
-	}
-}
-
-
-func suck(ch1,ch2 chan int){
-	for {
-		select {
-		case v:= <-ch1:
-			fmt.Print("Received on channel1:%d\n",v)
-		case v := <-ch2:
-			fmt.Print("Received on channel2:%d\n",v)
-		}
-	}
+	c := C{}
+	c.A.Name = "xx"
+	c.B.Name = "b"\
+	c.
 }
