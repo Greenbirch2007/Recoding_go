927cace8fc20e0f5e09965fe7450e587399655d8
Greenbirch2007 <291109028@qq.com>
2019-12-25 18:17:49 +0800

    struct类型

--- to/g1.go
+++ to/g1.go
@@ -1,29 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"time"
+import "fmt"
 
-func main()  {
-	fmt.Println("*******服务器启动，监听端口 8080*")
-	mux := http.NewServeMux()
-
-	//处理静态资源文件
-	files := http.FileServer(http.Dir("./public"))
-	mux.Handle("/static/",http.StripPrefix("/static",files)
-	mux.HandleFunc("/",index)
-
-	//配置服务器
-	server := &http.Server{
-		Addr:"0.0.0.0:8080",
-		Handler: mux, //设置多路复用器
-		ReadTimeout:time.Duration(10*int64(time.Second)),
-		WriteTimeout: time.Duration(200*int64(time.Second)),
-		MaxHeaderBytes:1 <<20, //左移运算，等同于1*2^20,高性能乘法运算
-		}
-		server.ListenAndServe()
+func max(a,b int)int  {
+	if a>b {
+		return a
+	}
+	return b
 }
-func index(writer http.ResponseWriter,request *http.Request)  {
-	fmt.Fprintf(writer,"hello world!")
-})
+
+
+func main(){
+	x := 3
+	y := 4
+	z := 5
+	max_xy := max(x,y)
+	max_xz := max(x,z)
+
+	fmt.Println("max(%d,%d) =%d\n",x,y,max_xy)
+	fmt.Println("max(%d,%d) =%d\n",x,z,max_xz)
+	fmt.Println("max(%d,%d) =%d\n",y,z,max(y,z))
+}
