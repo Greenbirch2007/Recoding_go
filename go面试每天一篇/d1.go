ffa43baff9c70370d7afd66913eee4e86157f1db
Greenbirch2007 <291109028@qq.com>
2020-01-07 20:11:45 +0800

    go面试题第8天

--- go面试每天一篇/d1.go
+++ go面试每天一篇/d1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+)
 func defer_call()  {
 	defer func(){fmt.Println("打印前")}()
 	defer func(){fmt.Println("打印中")}()
--- to/g1.go
+++ to/g1.go
@@ -1,32 +1,12 @@
 package main
 
-
-import (
-	"fmt"
-	"net/http"
-)
-
-type MyMux struct{
-
-}
-
-
-func (p *MyMux) ServeHTTP(w http.ResponseWriter,r *http.Request){
-	if r.URL.Path == "/"{
-		sayhelloName(w,r)
-		return
-	}
-	http.NotFound(w,r)
-	return
-}
-
-func sayhelloName(w http.ResponseWriter,r *http.Request)  {
-	fmt.Fprintf(w,"Hello myroute!")
+func GetValue() int  {
+	return 1
 }
 
 func main()  {
-	mux := &MyMux{}
-	http.ListenAndServe(":9090,mux")
+	i := GetValue()
+	switch i.(type){
+	case int:
+	}
 }
-
-
