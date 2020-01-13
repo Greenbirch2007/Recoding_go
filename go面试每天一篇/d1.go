06d85ab7fdc172d0cc134a7422de8edb95a55549
Greenbirch2007 <291109028@qq.com>
2020-01-13 15:44:57 +0800

    Go 面试每天一篇（第 42天）

--- go面试每天一篇/d1.go
+++ go面试每天一篇/d1.go
@@ -1,21 +1,15 @@
 package main
 
-import "fmt"
 
 
-func main()  {
-	var m = map[string]int{
-		"A":21,
-		"B":22,
-		"C":23,
-	}
-	counter := 0
-	for k,v := range m{
-		if counter == 0{
-			delete(m,"A")
-		}
-		counter++
-		fmt.Println(k,v)
-	}
-	fmt.Println("counter is ",counter)
+type Param map[string]interface{}
+
+type Show struct{
+	*Param
+}
+
+
+func main(){
+	s := new(Show)
+	s.Param["day"] = 2
 }
