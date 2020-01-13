c7f6fe03c9b7c8b0a595c7290209b27d2f2652e1
Greenbirch2007 <291109028@qq.com>
2020-01-13 16:48:34 +0800

    1.6.4 map

--- go面试每天一篇/d1.go
+++ go面试每天一篇/d1.go
@@ -1,15 +1,11 @@
 package main
 
-
-
-type Param map[string]interface{}
-
-type Show struct{
-	*Param
-}
-
+import "fmt"
 
 func main(){
-	s := new(Show)
-	s.Param["day"] = 2
+	str := "hello ,世界" // 通过字符串字面量初始化一个字符串str
+	a := []byte(str) // 将字符串转换为[]byte类型切片
+	b := []rune(str) // 将字符串转换为[]rune 类型切片
+	fmt.Println(a)
+	fmt.Println(b)
 }
