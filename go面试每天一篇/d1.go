2354d1956c4b4169af0e70cfa5261c45e5665b90
Greenbirch2007 <291109028@qq.com>
2020-01-14 11:49:57 +0800

    Go 面试每天一篇（第44天）

--- go面试每天一篇/d1.go
+++ go面试每天一篇/d1.go
@@ -1,11 +1,12 @@
 package main
 
-import "fmt"
+
 
 func main(){
-	str := "hello ,世界" // 通过字符串字面量初始化一个字符串str
-	a := []byte(str) // 将字符串转换为[]byte类型切片
-	b := []rune(str) // 将字符串转换为[]rune 类型切片
-	fmt.Println(a)
-	fmt.Println(b)
+	x := interface{}(nil)
+	y := (*int)(nil)
+	a := y == x
+	b := y ==nil
+	_,c := x.(interface{})
+	println(a,b,c)
 }
