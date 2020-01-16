7c33b9abf4adc066838ccad78d4c273846e6f54e
Greenbirch2007 <291109028@qq.com>
2020-01-16 10:39:39 +0800

    Go 面试每天一篇（６１天）

--- go面试每天一篇/d1.go
+++ go面试每天一篇/d1.go
@@ -2,14 +2,34 @@ package main
 
 import  "fmt"
 
-
-type T struct{
-	n int
+type data struct{
+	*sync.Mutex // *Mutex
 }
 
 
+func (d data) test(s string){
+	d.Lock()
+	defer d.Unlock()
+
+
+	for i:=0;i<5;i++{
+		fmt.Println(s,i)
+		time.Sleep(time.Second)
+	}
+}
+
 func main(){
-	m := make(map[int]T)
-	m[0].n = 1
-	fmt.Println(m[0].n)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	d := data{new(sync.Mutex)} // 初始化
+	go func(){
+		defer wg.Done()
+		d.test("read")
+	}()
+	go func(){
+		defer wg.Done()
+		d.test("write")
+	}()
+	wg.Wait()
 }
