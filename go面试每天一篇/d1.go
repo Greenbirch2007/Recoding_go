00caa8b2111e922b8ccfe3dd1990e747c86e133a
Greenbirch2007 <291109028@qq.com>
2020-01-20 11:31:47 +0800

    3.4.2 组合的方法集

--- go面试每天一篇/d1.go
+++ go面试每天一篇/d1.go
@@ -1,20 +1,41 @@
 package main
 
-import _ "fmt"
+
+import "fmt"
 
 
+type Int int
 
-func alwaysFalse() bool{
-	return false
+func (a Int) Max(b Int) Int{
+	if a>b{
+		return a
+	}else{
+		return b
+	}
 }
 
 
+func (i *Int) Set(a Int){
+	*i =a
+}
+
+func (i Int) Print(){
+	fmt.Printf("value=%d\n",i)
+}
+
 func main(){
-	switch alwaysFalse()
-	{
-	case true:
-		println(true)	
-	case false:
-		println(false)	
-	}
+	var a Int = 10
+	var b Int =20
+	
+	c := a.Max(b)
+	c.Print() //value=50
+
+	(&c).Print() //value=50,内部被编译器转换为c.Print()
+
+	a.Set(20) //内部被编译器转化为(&a).Set(20)
+	a.Print() // value=20
+	(&a).Set(30)
+
+
+	a.Print)// value=30
 }
