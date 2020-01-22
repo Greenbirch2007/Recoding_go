3fa03dfe6fb56bd17ccf98e61f471dab094c031b
Greenbirch2007 <291109028@qq.com>
2020-01-22 10:16:53 +0800

    4.3.3 空接口和nil

--- go面试每天一篇/d1.go
+++ go面试每天一篇/d1.go
@@ -1,22 +1,57 @@
 package main
 
-type Printer interface{
-	Print()
-}
-
-type S struct{}
+import "fmt"
 
 
-func (s S) Print(){
-	println("print")
+type Inter interface{
+	Ping()
+	Pang()
 }
 
 
-func main(){
-	var i Printer
+type Anter interface{
+	Inter
+	String()
+}
 
-	//没有初始化的接口调用其方法会产生panic
-	//必须初始化
-	i = S{}
-	i.Print()
+
+type St struct{
+	Name string
+}
+
+
+func (St) Ping(){
+	println("ping")
+}
+
+
+func (*St) Pang(){
+	println("pang")
+}
+
+
+f,err :=os.OpenFile("notes.txt",os.O_RDWR|os.O_CREATE,0755)
+if err != nil{
+	log.Fatal(err)
+}
+
+
+defer f.Close()
+
+var i io.Reader = f
+
+
+switch v:=i.(type){
+	//i的绑定的实例*osFile类型，实现了io.ReadWriter接口，
+	//所以case匹配成功
+case io.ReadWriter:
+	//v 是io.ReadWriter接口类型，所以可以调用Write方法
+	v.Witer([]byte("io.ReadWriter\n"))
+//由于上一个case已经匹配，就算这个case也匹配，也不会走到这里
+
+case *os.File:
+	v.Write([]byte("*os.File\n"))
+	v.Sync()
+default:
+	return	
 }
--- to/g1.go
+++ to/g1.go
@@ -2,6 +2,49 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Printf("HELLO J~\n")
+
+type Inter interface{
+	Ping()
+	Pang()
+}
+
+
+type Anter interface{
+	Inter
+	String()
+}
+
+
+type St struct{
+	Name string
+}
+
+
+func (St) Ping(){
+	println("ping")
+}
+
+
+func (*St) Pang(){
+	println("pang")
+}
+
+
+func main(){
+	st := &St{"andes"}
+	var i interface{} =st
+
+	if o,ok := i.(Inter);ok{
+		o.Ping()
+		o.Pang()
+	}
+
+	if p,ok := i.(Anter);ok{
+		p.String()
+
+	}
+
+	if s,ok := i.(*St);ok{
+		fmt.Printf("%s",s.Name)
+	}
 }
