7484e581b81f0cb9c480fa296dc81594c6f85509
Greenbirch2007 <291109028@qq.com>
2020-01-27 00:11:45 +0800

    5.1.7 通知退出机制

--- go面试每天一篇/d1.go
+++ go面试每天一篇/d1.go
@@ -1,57 +1,19 @@
 package main
 
-import "fmt"
 
+func main(){
+	ch := make(chan int,1)
+	go func(chan int){
+		for {
+			select {
+				// 0或1的写入是随机的
+			case ch <- 0:
+			case ch <-1:
 
-type Inter interface{
-	Ping()
-	Pang()
-}
-
-
-type Anter interface{
-	Inter
-	String()
-}
-
-
-type St struct{
-	Name string
-}
-
-
-func (St) Ping(){
-	println("ping")
-}
-
-
-func (*St) Pang(){
-	println("pang")
-}
-
-
-f,err :=os.OpenFile("notes.txt",os.O_RDWR|os.O_CREATE,0755)
-if err != nil{
-	log.Fatal(err)
-}
-
-
-defer f.Close()
-
-var i io.Reader = f
-
-
-switch v:=i.(type){
-	//i的绑定的实例*osFile类型，实现了io.ReadWriter接口，
-	//所以case匹配成功
-case io.ReadWriter:
-	//v 是io.ReadWriter接口类型，所以可以调用Write方法
-	v.Witer([]byte("io.ReadWriter\n"))
-//由于上一个case已经匹配，就算这个case也匹配，也不会走到这里
-
-case *os.File:
-	v.Write([]byte("*os.File\n"))
-	v.Sync()
-default:
-	return	
+			}
+		}
+	}(ch)
+	for i:= 0;i<10;i++{
+		println(<-ch)
+	}
 }
