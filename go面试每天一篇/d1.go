29825cebfd15c9308ed6f5b9865a34df24c36110
Greenbirch2007 <291109028@qq.com>
2020-02-21 12:14:45 +0800

    Go 面试每天一篇（第 104 天）

--- go面试每天一篇/d1.go
+++ go面试每天一篇/d1.go
@@ -1,48 +1,21 @@
 package main
 
-
-import  "fmt"
-import "sync"
-
-type UserAges struct{
-	ages map[string]int
-	sync.Mutex
-}
-
-func (ua *UserAges) Add(name string,age int){
-	ua.Lock()
-	defer ua.Unlock()
-	ua.ages[name] = age
-}
-
-func (ua *UserAges) Get(name string)int{
-	if age,ok := ua.ages[name];ok{
-		return age
-	}
-	return -1
-}
+import "fmt"
 
 
 func main(){
-	count := 1000
-	gw := sync.WaitGroup{}
-	gw.Add(count*3)
-	u := UserAges{ages:map[string]int{}}
-	add := func(i int){
-		u.Add(fmt.Sprintf("user_%d",i),i)
-		gw.Done()
-	}
+	fmt.Println(doubleScore(0))
+	fmt.Println(doubleScore(20.0))
+	fmt.Println(doubleScore(50.0))
+}
 
-	for i:=0;i < count;i++{
-		go add(i)
-		go add(i)
-	}
-	for i := 0;i<count;i++{
-		go func(i int){
-			defer gw.Done()
-			u.Get(fmt.Sprintf("user_%d".i))
-		}(i)
-	}
-	gw.Wait()
-	fmt.Println("Done")
+
+func doubleScore(source float32) (score float32) {
+	defer func(){
+		if score < 1|| score >= 100{
+			score = source
+		}
+	}()
+
+	return source *
 }
