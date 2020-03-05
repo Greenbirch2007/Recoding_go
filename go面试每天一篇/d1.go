fdb0aba234916952aa3b39b3a0f24f6b0f042543
Greenbirch2007 <291109028@qq.com>
2020-03-05 18:15:05 +0800

    准备softstone入职

--- go面试每天一篇/d1.go
+++ go面试每天一篇/d1.go
@@ -1,77 +1,51 @@
 package main
 
-import (
-    "fmt"
-    "sync"
-    "time"
-)
 
-func main() {
-    done := make(chan bool, 6)
 
-    var rw sync.RWMutex
+import "fmt"
 
-    go func() {
-        rw.RLock()
-        fmt.Println("Enter goroutine 1")
-        time.Sleep(time.Second * 5)
-        fmt.Println("Exit goroutine 1")
-        rw.RUnlock()
 
-        done <- true
-    }()
+type Person struct{
+    age int
+}
 
-    go func() {
-        rw.RLock()
-        fmt.Println("Enter goroutine 2")
-        time.Sleep(time.Second * 5)
-        fmt.Println("Exit goroutine 2")
-        rw.RUnlock()
 
-        done <- true
-    }()
+func (p Person) Elegance() int{
+    return p.age
+}
 
-    go func() {
-        rw.RLock()
-        fmt.Println("Enter goroutine 3")
-        time.Sleep(time.Second * 5)
-        fmt.Println("Exit goroutine 3")
-        rw.RUnlock()
 
-        done <- true
-    }()
+func (p *Person) GetAge(){
+    p.age += 1
 
-    go func() {
-        rw.Lock()
-        fmt.Println("Enter goroutine 4")
-        time.Sleep(time.Second * 5)
-        fmt.Println("Exit goroutine 4")
-        rw.Unlock()
+}
 
-        done <- true
-    }()
 
-    go func() {
-        rw.Lock()
-        fmt.Println("Enter goroutine 5")
-        time.Sleep(time.Second * 5)
-        fmt.Println("Exit goroutine 5")
-        rw.Unlock()
+func main(){
+    //p1 是值类型
+    
+    p := Person{age:18}
 
-        done <- true
-    }()
-    go func() {
-        rw.Lock()
-        fmt.Println("Enter goroutine 6")
-        time.Sleep(time.Second * 5)
-        fmt.Println("Exit goroutine 6")
-        rw.Unlock()
+    //值类型　调用接收者也是值类型的方法
 
-        done <- true
-    }()
 
-    for i := 0; i < 6; i++ {
-        <-done
-    }
+    fmt.Println(p.howOld())
 
+    //值类型　　调用接收者是指针类型的方法
+
+    p.GetAge()
+
+    fmt.Println(p.GetAge())
+
+    //p2 是指针类型
+
+    //指针类型　调用接收者是值类型的方法
+
+    fmt.Println(p2.GetAge())
+
+    //指针类型　调用接收者也是指针类型的方法
+
+
+    p2.GetAge()
+    fmt.Println(p2.GetAge())
 }
